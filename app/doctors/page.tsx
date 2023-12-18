b9cf54be3272a70ad09b4c23e2ca7e8f945d9c3a
Kastuell <sholotun@mail.ru>
2023-12-18 17:06:24 +1000

    123

--- app/doctors/page.tsx
+++ app/doctors/page.tsx
@@ -1,6 +1,6 @@
 import getDoctors from "@/actions/getDoctors";
 import { toggle } from "@/actions/updateDoctors";
-import { Star } from "lucide-react";
+import ChangePost from "@/components/changee";
 
 export default async function Doctors() {
   // const img = "../doctor.jpg";
@@ -33,38 +33,15 @@ export default async function Doctors() {
 
   // }
 
-// @ts-ignore
+  // @ts-ignore
   const tgl = (is_favorite: any, id: any) => {
     toggle(is_favorite, id)
   }
 
-  
+
   const data = await getDoctors()
   console.log(data)
   return (
-    <div className="container">
-      <div>
-        <h1 className=" text-4xl">Наши Врачи</h1>
-        <div className="mt-10 grid grid-cols-4 gap-4 lg+:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm+:grid-cols-1 text-center">
-          {data?.map((doctor:any) => (
-            <div id={doctor.id}>
-              <div className="flex flex-col   w-[250px] border border-maincolor rounded-t-[25px] overflow-hidden">
-                <div className="">
-                  <img className="w-[250px]" src={doctor.img} alt="" />
-                </div>
-                <div className="mt-[10px]">
-                  <div>{doctor.name.split(" ").slice(0, 1).join(" ")}</div>
-                  <div>{doctor.name.split(" ").slice(1, 3).join(" ")}</div>
-                </div>
-                <div>{doctor.special}</div>
-                <div className="mt-[20px] p-5 bg-maincolor">Стаж: {doctor.stage}</div>
-                 {/* @ts-ignore  */}
-                <button onClick={tgl(doctor.is_favorite, doctor.id)}>{doctor.is_favorite == false ? <Star /> : <Star color="yellow" />}</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
+    <ChangePost />
   );
 }
