1a7afd3dbde0097dd54d6a55d0cba81e757a1a0b
Kastuell <sholotun@mail.ru>
2023-12-19 15:44:54 +1000

    qwe

--- app/doctors/page.tsx
+++ app/doctors/page.tsx
@@ -1,5 +1,5 @@
+"use server"
 import getDoctors from "@/actions/getDoctors";
-import { toggle } from "@/actions/updateDoctors";
 import ChangePost from "@/components/changee";
 
 export default async function Doctors() {
@@ -33,15 +33,31 @@ export default async function Doctors() {
 
   // }
 
-  // @ts-ignore
-  const tgl = (is_favorite: any, id: any) => {
-    toggle(is_favorite, id)
-  }
-
-
   const data = await getDoctors()
   console.log(data)
   return (
-    <ChangePost />
+    <div className="container">
+            <div>
+                <h1 className=" text-4xl">Наши Врачи</h1>
+                <div className="mt-10 grid grid-cols-4 gap-4 lg+:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm+:grid-cols-1 text-center">
+                    {data?.map((doctor: any) => (
+                        <div key={doctor.id}>
+                            <div className="flex flex-col   w-[250px] border border-maincolor rounded-t-[25px] overflow-hidden">
+                                <div className=" basis-[25%]">
+                                    <img className="w-[250px]" src={doctor.img} alt="" />
+                                </div>
+                                <div className="mt-[10px] basis-[25%]">
+                                    <div>{doctor.name.split(" ").slice(0, 1).join(" ")}</div>
+                                    <div>{doctor.name.split(" ").slice(1, 3).join(" ")}</div>
+                                </div>
+                                <div className=" basis-[25%]">{doctor.special}</div>
+                                <div className="mt-[20px] p-5 bg-maincolor basis-[25%]">Стаж: {doctor.stage}</div>
+                                <ChangePost doctorId={doctor.id} doctorFavorite={doctor.favorite} />
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </div>
   );
 }
--- components/changee.tsx
+++ components/changee.tsx
@@ -1,67 +1,40 @@
 'use client'
 
-import getDoctors from "@/actions/getDoctors"
 import "easymde/dist/easymde.min.css"
+import { Star } from "lucide-react"
 import { useState } from 'react'
-import { supabase } from './api'
-
-
-const initialState = { id: 0, is_favorite: false }
 
 
 
-async function ChangePost() {
-    const data = await getDoctors()
 
-    const [post, setPost] = useState(initialState)
-    const { id, is_favorite } = post
+function ChangePost({ doctorId, doctorFavorite }: any) {
+
+    const [favorite, setFavorite] = useState<boolean>(doctorFavorite)
+    // const { id, favorite } = post
     // @ts-ignore
-    async function createNewPost(id: any, is_favorite: any) {
-        setPost(() => ({ ...post, [is_favorite]: !is_favorite, [id]: id }))
-        if (post) return
-        // const user = supabase.auth.user()
-        // const id = uuid()
-        // post.id = id
-        // const { data } = await supabase
-        //     .from('doctor')
-        //     .insert([
-        //         { name, special, stage }
-        //     ])
-        //     .single()
-        const { error } = await supabase
-            .from('doctor')
-            .update({ is_favorite: is_favorite })
-            .eq('id', id)
-        if (error) {
-            console.log(error);
-        }
-        console.log(post)
-    }
+    // async function changePoste(id: any) {
+    //     setFavorite((prev:boolean) => !prev)
+    //     const { data, error } = await supabase
+    //         .from('doctor')
+    //         .update({ favorite: true })
+    //         .eq( 'id', id )
+    //         .select()
+    //     if (error) {
+    //         console.log(error);
+    //     }
+    //     console.log(favorite)
+    //     if(data){
+    //         console.log(data)
+
+    //     }
+    // }
     return (
-        <div className="container">
-            <div>
-                <h1 className=" text-4xl">Наши Врачи</h1>
-                <div className="mt-10 grid grid-cols-4 gap-4 lg+:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm+:grid-cols-1 text-center">
-                    {data?.map((doctor: any) => (
-                        <div id={doctor.id}>
-                            <div className="flex flex-col   w-[250px] border border-maincolor rounded-t-[25px] overflow-hidden">
-                                <div className="">
-                                    <img className="w-[250px]" src={doctor.img} alt="" />
-                                </div>
-                                <div className="mt-[10px]">
-                                    <div>{doctor.name.split(" ").slice(0, 1).join(" ")}</div>
-                                    <div>{doctor.name.split(" ").slice(1, 3).join(" ")}</div>
-                                </div>
-                                <div>{doctor.special}</div>
-                                <div className="mt-[20px] p-5 bg-maincolor">Стаж: {doctor.stage}</div>
-                                {/* @ts-ignore  */}
-                                <button onClick={createNewPost(doctor.id, doctor.is_favorite)}>{doctor.is_favorite == false ? <Star /> : <Star color="yellow" />}</button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            </div>
-        </div>
+        <>
+            {/* <button onClick={() => changePoste(doctorId)} > {doctorFavorite == false ? <Star /> : <Star color="yellow" />}</button > */}
+            <button onClick={() => setFavorite((prev: any) => !prev)} > {favorite == false ? <Star /> : <Star color="yellow" />}</button >
+            {/* <input type="checkbox" value='true' onChange={()=> changePoste(doctorId)} /> Избранное */}
+        </>
+
     )
 }
 
